d7542b98a718f926610418ecee2d521bb239ce50
Caleb <calebh101dev@icloud.com>
2025-06-02 22:25:44 -0500

    Update the home app launcher to be more robust and to log output

--- tools/backgroundapp.sh
+++ tools/backgroundapp.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-(sleep 5 && /var/www/home/build/homeapp --kiosk) > /dev/null 2>&1 &
+# A small script to launch and manage the built home app in the background.
+
+root="/var/www/home"
+logfile="$root/logs/homeapp.log"
+kioskarg=""
+
+echo "Log file cleared!" > "$logfile"
+
+log() {
+    while IFS= read -r line; do
+        echo "$(date -Iseconds): $line" >> "$logfile"
+    done
+}
+
+(
+    sleep 5
+    echo "Process starting..." | log
+    "$root/build/homeapp" $kioskarg 2>&1 | log
+    echo "Process complete" | log
+) &> /dev/null &
